394f4dedadaf22dbcc9a480b13da528c01b4443d
emma deas <emmadeas9@gmail.com>
2018-10-30 06:50:52 -0700

    pass more specs in mancala

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -16,12 +16,13 @@ class Board
       end
       i += 1
     end
+    @cups[13] = []
     cups
     # helper method to #initialize every non-store cup with four stones each
   end
 
   def valid_move?(start_pos)
-    unless start_pos >= 0 && start_pos < 15
+    unless start_pos >= 0 && start_pos < 14
       raise 'Invalid starting cup'
     end
     raise 'Starting cup is empty' if @cups[start_pos].empty?
@@ -31,6 +32,7 @@ class Board
   def make_move(start_pos, current_player_name)
     if valid_move?(start_pos)
       stones = @cups[start_pos].length
+      num = stones
       @cups[start_pos] = []
       i = start_pos + 1
       while stones > 0
@@ -41,15 +43,24 @@ class Board
           @cups[i] << :stone
           stones -= 1
         end
+        if i == 13
+          i = 0
+        end
         i += 1
       end
     end
     render
-    next_turn(start_pos + stones)
+    next_turn(num)
   end
 
   def next_turn(ending_cup_idx)
-
+    if @cups[ending_cup_idx].length > 1 && ending_cup_idx.between?(6, 13)
+      :switch
+    elsif @cups[ending_cup_idx].length > 1 && ending_cup_idx.between?(0, 6)
+      :prompt
+    else
+      ending_cup_idx
+    end
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
