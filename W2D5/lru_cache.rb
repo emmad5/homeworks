ec0f3c8fc310f3e1d15820a136dfb6e2afee3ab3
emma deas <emmadeas9@gmail.com>
2018-11-02 06:11:33 -0700

    Complete lru cache homework

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,16 +1,24 @@
 class LRUCache
-  def initialize
+  def initialize(length)
+    @cache = []
+    @length = length
   end
 
   def count
+    @cache.size
     # returns number of elements currently in cache
   end
 
   def add(el)
+    unless @cache.inclde?(el)
+      @cache.shift if @cache.count < length
+      @cache.push(el)
+    end
     # adds element to cache according to LRU principle
   end
 
   def show
+    p @cache
     # shows the items in the cache, with the LRU item first
   end
 
