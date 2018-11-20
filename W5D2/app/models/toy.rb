4d0dc85277cb2ae2f09040ffb6a27f93e9cf41af
emma deas <emmadeas9@gmail.com>
2018-11-19 21:26:24 -0800

    add table Toys

--- W5D2/app/models/toy.rb
+++ W5D2/app/models/toy.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  toyable_type :string
+#  toyable_id   :bigint(8)
+#
+
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
